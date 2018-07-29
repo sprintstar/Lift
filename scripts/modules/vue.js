4a70ca68cdf0e09fb1bd2d4752370a78f46c4612
Chris Percival <chris@fastbug.net>
2018-07-29 19:23:25 +0100

    Actually added vue.js

---
+++ scripts/modules/vue.js
@@ -0,0 +1,8 @@
+define(["require", "exports"], function (require, exports) {
+    var Vue = (function () {
+        function Vue() {
+        }
+        return Vue;
+    })();
+    exports.Vue = Vue;
+});
---
+++ scripts/modules/vue.ts
@@ -0,0 +1,10 @@
+export class Vue {
+    constructor() {
+        var app2 = new Vue({
+            el: '#app-2',
+            data: {
+                message: 'You loaded this page on ' + new Date().toLocaleString()
+            }
+        })
+    }
+}
